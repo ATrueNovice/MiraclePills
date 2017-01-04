6005163fa27b0dbaf604b15b641d3a94995d9d3b
ATrueNovice <sean.haughtonscott@gmail.com>
2017-01-03 19:28:46 -0500

    Second Wave

--- miraclepills/miraclePill/miraclePill/ViewController.swift
+++ miraclepills/miraclePill/miraclePill/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "New York", "California", "Maine", "Nevada", "Colorado", "Michigan", "Florida", "North Carolina", "South Carolina", "Georgia", "New Jersey", "Delaware", "Vermont"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Tells code that the data source and the delegate is within this code.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func stateBtnPressed(_ sender: Any) {
+    
+    }
+    // Columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //How many items are going to show up. States.Count = Array.How many items are in the array.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    //returns the states in the array by name.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 }
 
